099d6b55e239364f42a2c893a44e93e1811b4d73
Gamaremy <148548581+Gamaremy@users.noreply.github.com>
2023-11-03 16:05:22 +0100

    Création des fichiers
    Création du fichier main.py -> Programme qui testera le scrpit Duree_classe.py
    Création du fichier Duree_Classe.py  -> Création d'une classe de gestion du temps

---
+++ Duree_Classe.py
@@ -0,0 +1,14 @@
+# Exercice
+# Coder une classe Durée possédant trois attributs :
+# ·   Le nombre d’heures de la durée ;
+# ·   Le nombre de minutes de la durée ;
+# ·   Le nombre de secondes de la durée.
+# Et les méthodes publiques suivantes :
+# ·   Un constructeur par défaut (tous les attributs auront zéro comme valeur) et un constructeur initialisant toutes les valeurs. On vérifiera que les nombres d’heures, minutes, secondes sont bien positifs, dans le cas contraire on considèrera leurs opposés. On fera également les conversions nécessaires pour que les nombres de minutes et de secondes soient strictement inférieurs à 60.
+# ·   Une méthode réalisant l’affichage de la durée (sous la forme 3h10m00s) ;
+# ·   Une méthode convertissant la durée en un nombre de secondes ;
+# ·   Une méthode rajoutant à la durée un nombre de secondes.
+
+#Pensez à tester toute votre classe dans un main.
+#Dépôt par personne avec une branche main puis une branche dev qui dérive de main, vous codez sur la branche dev et une fois le code fini, vous merguez dev sur main. ATTENTION commencer par protéger votre branche main en imposant un pull request pour merger dessus.
+
---
+++ main.py
@@ -0,0 +1,4 @@
+def main():
+	print('création du code')
+if __name__ == '__main__':
+	main()
