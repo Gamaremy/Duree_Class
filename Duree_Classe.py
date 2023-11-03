2801c45a521d343812fc7056c317ca87bf9f3440
Gamaremy <148548581+Gamaremy@users.noreply.github.com>
2023-11-03 17:04:30 +0100

    Création des programme
    Mise en place de la classe,
    Mise en place du programme main pour essayer toute les fonctions de la classe

--- Duree_Classe.py
+++ Duree_Classe.py
@@ -12,3 +12,42 @@
 #Pensez à tester toute votre classe dans un main.
 #Dépôt par personne avec une branche main puis une branche dev qui dérive de main, vous codez sur la branche dev et une fois le code fini, vous merguez dev sur main. ATTENTION commencer par protéger votre branche main en imposant un pull request pour merger dessus.
 
+# définition de la classe duree
+
+class Duree:
+	def __init__(self, heure=0,minute=0,seconde=0):
+		self.heure = heure
+		self.minute = minute
+		self.seconde = seconde
+		
+
+	def __str__(self):
+		return str(self.heure).zfill(2)+'h'+str(self.minute)+'m'+str(self.seconde).zfill(2)+'s'
+	
+	def afficher(self):
+		print(str(self.heure).zfill(2)+'h'+str(self.minute)+'m'+str(self.seconde).zfill(2)+'s')
+		
+	def conversion(self):
+		converti=self.heure * 3600 + self.minute * 60 + self.seconde
+		print("La durée en temps HH:MM:SS vaut : ", str(self.heure).zfill(2)+'h'+str(self.minute).zfill(2)+'m'+str(self.seconde).zfill(2)+'s')
+		print("La durée en seconde vaut : ", self.heure * 3600 + self.minute * 60 + self.seconde,' seconde')
+		return converti
+	
+	def ajouter(self, secondes_ajouter=0):
+		total = self.conversion()
+		total += secondes_ajouter
+		self.heure = total // 3600  # Quotient pour les heures
+		total %= 3600  # Reste après avoir ajouté les heures
+		self.minute = total // 60  # Quotient pour les minutes
+		self.seconde = total % 60  # Reste pour les secondes
+		return print('la durée nouvelle vaut : ',str(self.heure).zfill(2)+'h'+str(self.minute).zfill(2)+'m'+str(self.seconde).zfill(2)+'s')
+        
+        
+
+	
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import Duree_Classe as DC
+
+
+
 def main():
-	print('création du code')
+	MonTempsTest=DC.Duree(5,20,30)
+	# Fonction afficher()
+	MonTempsTest.afficher()
+	#Ou marche avec juste le nom de la variable grace à la méthode __str_
+	print(MonTempsTest)
+	MonTempsTest.conversion()
+	MonTempsTest.ajouter(3600)
+	
+
+
+    
+	
 if __name__ == '__main__':
 	main()
